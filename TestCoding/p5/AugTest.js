14cae3a177c725d47cf845469cd98cf40207afee
Jonathan <JonVu98@gmail.com>
2019-09-11 13:41:18 -0500

    update

---
+++ TestCoding/p5/AugTest.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+     
+}
+ 
+function draw(){
+    background(200,0,500);
+    rectangle(50,100)
+}
+
+
+
+function windowResized (){
+    resizeCanvas(windowWidth,windowHeight);
+}
